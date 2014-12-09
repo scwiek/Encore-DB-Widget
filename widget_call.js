0a37b18730058eee5d404851dd7a376ef06d4bf5
Sean Cwiek <cwieks@mcls.org>
2014-12-09 15:34:44 -0500

    phase 2 added call

--- widget_call.js
+++ widget_call.js
@@ -1,3 +1,4 @@
+<script src="http://elibrary.mel.org/staging/screens/widget.js"></script>
 <script type="text/javascript">
 $j(document).ready(function() {
 	if(document.URL.indexOf("/iii/encore/plus/")){
